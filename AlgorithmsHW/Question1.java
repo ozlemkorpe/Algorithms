5103abd5b2c206700c278580ed24ea515e7da691
Ozlem <cse.ozlemkorpe@gmail.com>
2021-01-09 18:52:12 +0300

    HW Init

---
+++ AlgorithmsHW/Question1.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+Title: Algorithms Question 1
+Author: @OzlemKorpe
+Date: 09.01.2021
+*******************************************************************************/
+
+/*
+You are given a sorted array A of n distinct integers, drawn from 1 to m where
+n<m. That is, A is a subset of [1,2,...,m]. Implement an O(logn) time algorithm
+(i.e., write a function) to find the smallest non-negative integer that is 
+missing in A given as a parameter. For example, when A is [1, 2, 3, 5, 7, 8, 10],
+the function must return 4.
+*/
+
+public class Main
+{
+	public static void main(String[] args) {
+
+	}
+}
---
+++ AlgorithmsHW/Question2.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+Title: Algorithms Question 2
+Author: @OzlemKorpe
+Date: 09.01.2021
+*******************************************************************************/
+
+/*
+Given an unsorted array, the array has this property that
+every element in array is at most k distance from its position
+in sorted array where k is a positive integer smaller than
+size of array. For example, let us consider k is 2, an element
+at index 7 in the sorted array, can be at indexes 5, 6, 7, 8, 9
+in the given array. We can sort such arrays more efficiently 
+with the help of Heap data structure. Implement the following
+algorithm that uses a Heap: - Create a Min Heap of size k+1 with
+first k+1 elements. - One by one remove min element from heap, 
+put it in result array, and add a new element to heap from 
+remaining elements.
+*/
+
+public class Main
+{
+	public static void main(String[] args) {
+
+	}
+}
