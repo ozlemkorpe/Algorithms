e4456e63405c91d2386875cc76fb93d65c626865
Ozlem <cse.ozlemkorpe@gmail.com>
2021-01-10 20:12:24 +0300

    Done

--- AlgorithmsHW/Question1.java
+++ AlgorithmsHW/Question1.java
@@ -5,16 +5,40 @@ Date: 09.01.2021
 *******************************************************************************/
 
 /*
-You are given a sorted array A of n distinct integers, drawn from 1 to m where
-n<m. That is, A is a subset of [1,2,...,m]. Implement an O(logn) time algorithm
-(i.e., write a function) to find the smallest non-negative integer that is 
-missing in A given as a parameter. For example, when A is [1, 2, 3, 5, 7, 8, 10],
-the function must return 4.
+Question 1. You are given a sorted array A of n distinct integers, drawn from 1 to m where n<m. 
+That is, A is a subset of [1,2,...,m]. Implement an O(logn) time algorithm (i.e., write a function)
+ to find the smallest non-negative integer that is missing in A given as a parameter. 
+For example, when A is [1, 2, 3, 5, 7, 8, 10], the function must return 4.
 */
 
-public class Main
-{
-	public static void main(String[] args) {
+class QuestionOne  { 
+    /* given a sorted array A of n distinct integers */
+    
+    public static void main(String[] args)  
+    { 
+        QuestionOne number = new QuestionOne(); 
+        int nNumbers[] = {1, 2, 5, 6, 7, 8, 10, 11, 12}; 
+        int length = nNumbers.length; 
+        System.out.println("Missing number is : " + number.firstMissing(nNumbers, 0, length - 1)); 
+    } 
+    
+    /* Binary Search method can be applied to obtain O(logn) time complexity */
+    int firstMissing(int array[], int first, int last)  
+    {   int missingNum = 0;
+        if (first > last){
+            missingNum = last + 1;
+            return missingNum; }
+        if (first != array[first]){
+             missingNum = first;
+            return missingNum;}
+        
+        int middle = (first + last) / 2; 
+  
+        /* If the middle value matches its index, then missing number must be on 
+        the right side of the middle index */
+        if (array[middle] == middle) { return firstMissing(array, middle+1, last);}
+        
+        return firstMissing(array, first, middle); 
+    } 
 
-	}
-}
+} 
--- AlgorithmsHW/Question2.java
+++ AlgorithmsHW/Question2.java
@@ -3,24 +3,41 @@ Title: Algorithms Question 2
 Author: @OzlemKorpe
 Date: 09.01.2021
 *******************************************************************************/
+import java.util.PriorityQueue;
+import java.util.Arrays;
+import java.util.List;
 
-/*
-Given an unsorted array, the array has this property that
-every element in array is at most k distance from its position
-in sorted array where k is a positive integer smaller than
-size of array. For example, let us consider k is 2, an element
-at index 7 in the sorted array, can be at indexes 5, 6, 7, 8, 9
-in the given array. We can sort such arrays more efficiently 
-with the help of Heap data structure. Implement the following
-algorithm that uses a Heap: - Create a Min Heap of size k+1 with
-first k+1 elements. - One by one remove min element from heap, 
-put it in result array, and add a new element to heap from 
-remaining elements.
+/* 
+Question 2. Given an unsorted array, the array has this property that every element
+in array is at most k distance from its position in sorted array where k is a positive
+integer smaller than size of array. For example, let us consider k is 2, an element at
+index 7 in the sorted array, can be at indexes 5, 6, 7, 8, 9 in the given array. We can
+sort such arrays more efficiently with the help of Heap data structure. Implement the
+following algorithm that uses a Heap:
+- Create a Min Heap of size k+1 with first k+1 elements.
+- One by one remove min element from heap, put it in result array, and add a new 
+element to heap from remaining elements.
 */
 
-public class Main
-{
-	public static void main(String[] args) {
-
-	}
-}
+class QuestionTwo{
+    // Function to sort a K-Sorted Array
+    public static void sortKSortedArray(List<Integer> list, int k){
+        int index = 0;
+        
+        // - Create a Min Heap of size k+1 with first k+1 elements.
+        PriorityQueue<Integer> myheap = new PriorityQueue<>(list.subList(0, k+1));
+        
+        /* For loop for One by one remove min element from heap, put it in result array, and add a new 
+element to heap from remaining elements. */
+        for (int i = k + 1; i < list.size(); i++){
+            // Pop the top min element from the heap and store it in the index
+            list.set(index++, myheap.poll());
+            // Push next element into the heap
+            myheap.add(list.get(i));
+        }
+ 
+        // Pop last elements and store it in the next indexes 
+        while (!myheap.isEmpty()) {
+            list.set(index++, myheap.poll());
+        }
+    }
